309175821742ab48d0fee91f0ce2a04b486b60ae
KevinIsMyName <wu.kevin.2520@gmail.com>
2020-05-04 21:42:47 -0400

    Setup script

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo -e "setup.sh assumes that you are running Python 3+ and have pip 19+ installed. If not, ctrl+c now!\n\n\n\n\n\n"
+rm -rf venv
+python -m venv venv
+./venv/Scripts/activate
+pip install -r requirements.txt
+echo -e -n "\n\n\n\n\n\nRun ExcelGrabber.py with the following commands: \n"
+echo -e -n "\t./venv/Scripts/activate  <--Done automatically after setup.sh\n"
+echo -e -n "\tpython ExcelGrabber.py <PATH_TO_ECHO_XLSX>\n"
+echo -e -n "See more information in README.md\n"
