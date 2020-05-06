3b57ace73cbe6ea29316c170c06d1dfce6aeaa89
KevinIsMyName <wu.kevin.2520@gmail.com>
2020-05-05 22:45:17 -0400

    Works on Linux

--- posix_setup.sh
+++ posix_setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-echo -e "setup.sh assumes that you are running Python 3+ and have pip 19+ installed. If not, ctrl+c now!\n\n\n\n\n\n"
+echo -e "posix_setup.sh assumes that you are running Python 3+ and have pip 19+ installed. If not, ctrl+c now!\n\n\n"
 rm -rf venv
-python -m venv venv
-./venv/Scripts/activate
+python3 -m venv venv
+source venv/bin/activate
 pip3 install -r requirements.txt
-echo -e -n "\n\n\n\n\n\nRun HyperionCSV.py with the following commands: \n"
-echo -e -n "\t./venv/Scripts/activate OR begin.sh (this is autorun after shell.sh)\n"
-echo -e -n "\tpython HyperionCSV.py <PATH_TO_ECHO_XLSX>\n"
+echo -e -n "\n\n\nRun HyperionCSV.py with the following commands: \n"
+echo -e -n "\tsource ./venv/Scripts/activate\n"
+echo -e -n "\tpython HyperionCSV.py PATH_TO_ECHO_XLSX\n"
 echo -e -n "See more information in README.md\n"
